5a5faad888e4884f936418c8f604c2819e505a69
cheesu <dlscltn@gmail.com>
2024-03-22 12:58:13 +0900

    scss 적용

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../app/globals.css";
+import "../app/globals.scss";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import Main from "@/components/layout/Main";
