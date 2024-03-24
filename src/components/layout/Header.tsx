f2c547bf5a5f2150634b4ecb4f0509b01e6bffc4
cheesu <dlscltn@gmail.com>
2024-03-25 08:56:03 +0900

    메뉴 컴포넌트 코드 개선

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -76,40 +76,6 @@ const Header = () => {
                     <nav className="pointer-events-auto hidden md:block">
                       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
                         <Menu />
-                        {/**
-                        <li>
-                          <a
-                            className="relative block px-3 py-2 transition text-teal-500 dark:text-teal-400"
-                            href="/submain"
-                          >
-                            submain
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                            href="/test/sub"
-                          >
-                            sub
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                            href="/speaking"
-                          >
-                            Speaking
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
-                            href="/uses"
-                          >
-                            Uses
-                          </a>
-                        </li>
-                         */}
                       </ul>
                     </nav>
                   </div>
--- src/components/layout/Menu.tsx
+++ src/components/layout/Menu.tsx
@@ -12,37 +12,36 @@ const Menu = () => {
   const { menuId } = useSelector((state: RootState) => state.menu);
   const [selectedMenuId, setSelectedMenuId] = useState(menuId);
 
-  const clickMenuHandler = useCallback((id: number) => {
-    dispatch(setMenuId(id));
-    setSelectedMenuId(id);
-  }, []);
+  const clickMenuHandler = useCallback(
+    (id: number) => {
+      dispatch(setMenuId(id));
+      setSelectedMenuId(id);
+    },
+    [dispatch]
+  );
 
   return (
     <>
-      {MENU_LIST?.map((item: MenuItem) => {
-        let underLine = <span></span>;
-        let menuStyle =
-          "relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400";
-        if (item.id === selectedMenuId) {
-          underLine = (
-            <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0"></span>
-          );
-          menuStyle =
-            "relative block px-3 py-2 transition text-teal-500 dark:text-teal-400";
-        }
-        return (
-          <li key={item.id}>
-            <Link
-              className={menuStyle}
-              href={item.url}
-              onClick={() => clickMenuHandler(item.id)}
-            >
-              {item.title}
-              {underLine}
-            </Link>
-          </li>
-        );
-      })}
+      {MENU_LIST?.map((item: MenuItem) => (
+        <li key={item.id}>
+          <Link
+            className={`relative block px-3 py-2 transition ${
+              item.id === selectedMenuId
+                ? "text-teal-500 dark:text-teal-400"
+                : "hover:text-teal-500 dark:hover:text-teal-400"
+            }`}
+            href={item.url}
+            onClick={() => {
+              clickMenuHandler(item.id);
+            }}
+          >
+            {item.title}
+            {item.id === selectedMenuId && (
+              <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0"></span>
+            )}
+          </Link>
+        </li>
+      ))}
     </>
   );
 };
